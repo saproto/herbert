6482b9a4355bf51f0c106016c88829353f375e36
Thijs Dortmann <thijs@thijsdortmann.nl>
2016-06-14 12:18:28 +0200

    Add keypad to remote, improve search.

--- public/remote/js/remote.js
+++ public/remote/js/remote.js
@@ -1,62 +1,99 @@
-var remote = io('/protube-remote');
-
-remote.on("connect", function() {
-    $("#connecting").hide(0);
-    $("#connected").show(0);
-});
-
-remote.on("queue", function(data) {
-    var queue = $("#queue");
-    queue.html("");
-    
-    for(var i in data) {
-        queue.append('<img src="http://img.youtube.com/vi/' + data[i].id + '/0.jpg" width="20%" />');
-    }
-});
-
 $(document).ready(function() {
-    $("#doLogin").click(function(e) {
-        e.preventDefault();
-        remote.emit("authenticate", { 'pin' : $("#pin").val() });
+    var errorElement = $("body");
+
+    var remote = io('/protube-remote');
+
+    remote.on("connect", function() {
+        $("#connecting").hide(0);
+        $("#connected").show(0);
+    });
+
+    remote.on("queue", function(data) {
+        var queue = $("#queue");
+        queue.html("");
+
+        for(var i in data) {
+            queue.append('<img src="http://img.youtube.com/vi/' + data[i].id + '/0.jpg" />');
+        }
     });
 
     $('form').bind('submit', function(e){
         e.preventDefault();
         remote.emit("search", $("#searchBox").val());
     });
-});
 
-remote.on("authenticated", function(data) {
-    if(data) {
-        $("#login").hide(0);
-        $("#loggedIn").show(0);
-    }else{
-        alert("Nope. Try again.");
-    }
-});
+    $("#pincode").html("");
 
-remote.on("searchResults", function(data) {
-    var results = $("#results");
+    $('.keyboard-key').on('click', function(){
+        errorElement.css({
+            "-moz-animation": "none",
+            "-webkit-animation": "none",
+            "animation": "none"
+        });
 
-    results.html("");
+        var pincode = $("#pincode");
 
-    for(var i in data.items) {
-        results.append(generateResult(data.items[i]));
-    }
+        if($(this).hasClass('back')) {
+            pincode.html( pincode.html().substring(0, pincode.html().length-1) );
+        } else {
+            if(pincode.html().length < 3) {
+                pincode.html( pincode.html() + $(this).html() );
+            }
+        }
 
-    $(".result").each(function(i) {
-        var current = $(this);
-        alert('yay');
-        current.click(function(e) {
-            e.preventDefault();
-            remote.emit("add", { id: current.attr("ytId")});
-        })
+        if( pincode.html().length == 3 ) {
+            remote.emit("authenticate", { 'pin' : pincode.html() });
+        }
+
+    });
+
+    $('body').on('keydown', function(event){
+        if( $('#login').is(':visible') ) {
+            if(event.keyCode >= 48 && event.keyCode <= 57 ) { // 0-9 normal
+                $('.keyboard-key:contains("' + (event.keyCode - 48) + '")').click();
+            } else if(event.keyCode >= 96 && event.keyCode <= 105 ) { // 0-9 normal
+                $('.keyboard-key:contains("' + (event.keyCode - 96) + '")').click();
+            } else if( event.keyCode == 8 ) { // backspace
+                $('.keyboard-key.back').click();
+            }
+        }
+    });
+
+    remote.on("authenticated", function(correct) {
+        if(correct) {
+            $("#login").hide(0);
+            $("#loggedIn").show(0);
+        }else{
+            $(errorElement).css({
+                "-moz-animation": "error 0.5s",
+                "-webkit-animation": "error 0.5s",
+                "animation": "error 0.5s"
+            });
+            $("#pincode").html("");
+        }
+    });
+
+    remote.on("searchResults", function(data) {
+        var results = $("#results");
+
+        results.html("");
+
+        for(var i in data.items) {
+            results.append(generateResult(data.items[i]));
+        }
+
+        $(".result").each(function(i) {
+            var current = $(this);
+            current.click(function(e) {
+                e.preventDefault();
+                remote.emit("add", { id: current.attr("ytId")});
+            })
+        });
     });
 });
 
 function generateResult(item) {
-    console.log(item);
-    var result = '<div class="result" ytId="' + item.id.videoId + '"><img src="http://img.youtube.com/vi/' + item.id.videoId + '/0.jpg" width="20%" /><h1>' + item.snippet.title + '</h1><h2>' + item.snippet.channelTitle +  '</h2></div>';
+    var result = '<div class="result" ytId="' + item.id.videoId + '"><img src="http://img.youtube.com/vi/' + item.id.videoId + '/0.jpg" width="20%" /><h1>' + item.snippet.title + '</h1><h2>' + item.snippet.channelTitle +  '</h2><div style="clear: both;"></div></div>';
 
     return result;
 }
