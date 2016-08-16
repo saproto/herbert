97eec153420e328414f46becb14495d967f8ba96
Thijs Dortmann <thijs@thijsdortmann.nl>
2016-08-16 18:22:33 +0200

    Add window definition loading.

--- herbert_modules/petra/petra.js
+++ herbert_modules/petra/petra.js
@@ -9,8 +9,30 @@ var ee = require('../../events');
 var io = require('../../io').socketio;
 var nsp = io.of('/petra');
 
+var protubeOn = true;
+
+var windowDefinitions = [{
+    "name": "protube",
+    "displayNumber": 1,
+    "url": "https://www.saproto.nl/protube/screen"
+},
+{
+    "name": "smartxp",
+    "displayNumber": 3,
+    "url": "https://www.saproto.nl/smartxp"
+},
+{
+    "name": "narrowcasting",
+    "displayNumber": 0,
+    "url": "https://www.saproto.nl/narrowcasting"
+}];
+
 nsp.on("connection", function(socket) {
     console.log("[petra] petra connected");
+
+    socket.on("get-window-definitions", function(data) {
+        socket.emit("loadPages", windowDefinitions);
+    });
 });
 
 ee.on("soundboard", function(data) {
